c2083d0f42ebc753c8fd78f84e39ba703d06d10f
FatihOzerw <53975463+FatihOzerw@users.noreply.github.com>
2020-05-22 22:23:18 +0200

    master

--- server.js
+++ server.js
@@ -42,11 +42,9 @@ if(process.env.NODE_ENV === 'production')
 {
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', function(_, res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
-      if (err) {
-        res.status(500).send(err)
-      }
-    })
+
+    res.sendFile(path.join(__dirname, 'client','build','index.html'));
+
   })}
 
 
