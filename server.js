93a95db9b66cb1d2e0687c7319450bb35873a600
Cobatsu <huze.ozr@gmail.com>
2020-05-24 09:33:48 +0200

    hey

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require('./routes');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const dotenv = require('dotenv');
-
+//
 dotenv.config();
 const _Url = process.env.DATABASE_URL;
 console.log(PORT);
