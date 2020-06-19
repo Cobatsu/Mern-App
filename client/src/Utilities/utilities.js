b5d8a7af89bf3df42bb79a3c6ce0716966cef9f7
Cobatsu <huze.ozr@gmail.com>
2020-06-19 14:07:09 +0200

    last

--- client/src/Utilities/utilities.js
+++ client/src/Utilities/utilities.js
@@ -10,16 +10,6 @@ export const restrictWord = (word,limit)=>{
 }
 
 
-<<<<<<< HEAD
-export const avoidUndefined = ()=>{
-
-
-
-}
-
-
-=======
->>>>>>> ce5ad5f773927d736c978d9869148b9b97015bec
 export const statusColors = (report) => {
 
     const { isContacted , isFormFilled , isFormSent } = report ; 
@@ -31,11 +21,7 @@ export const statusColors = (report) => {
 
             text: 'Beklemede' , 
             style:{
-<<<<<<< HEAD
                backgroundColor:'rgba(226, 151, 156, 0.25)' , 
-=======
-               backgroundColor:'rgba(226, 151, 156, 0.5)' , 
->>>>>>> ce5ad5f773927d736c978d9869148b9b97015bec
                border:'1px solid #e7305b',
                color:'#e7305b',
             }
@@ -49,11 +35,7 @@ export const statusColors = (report) => {
 
              text: 'Görüşme Yapıldı' , 
              style:{
-<<<<<<< HEAD
                 backgroundColor: 'rgba(135, 223, 214, .25)' , 
-=======
-                backgroundColor: '#87dfd6' , 
->>>>>>> ce5ad5f773927d736c978d9869148b9b97015bec
                 border:'1px solid #58b4ae',
                 color:'#58b4ae',
              }
@@ -66,11 +48,7 @@ export const statusColors = (report) => {
 
             text: 'Form Gönderildi' , 
             style:{
-<<<<<<< HEAD
                backgroundColor: 'rgba(249, 196, 154, .25)' , 
-=======
-               backgroundColor: '#f9c49a' , 
->>>>>>> ce5ad5f773927d736c978d9869148b9b97015bec
                border:'1px solid #f37121',
                color:'#f37121',
             }
@@ -83,15 +61,9 @@ export const statusColors = (report) => {
 
             text: 'Ön Kayıt' , 
             style:{
-<<<<<<< HEAD
                backgroundColor: 'rgba(0, 144, 158, .18)' , 
                border:'1px solid #00909e',
                color:'#00909e',
-=======
-               backgroundColor: '#d291bc' , 
-               border:'1px solid #b5076b',
-               color:'#b5076b',
->>>>>>> ce5ad5f773927d736c978d9869148b9b97015bec
             }
 
        }
